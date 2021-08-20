e1a28f9024d2c242add6295383a5f3a5bd7024b1
calic0cat <suzuki11235813@gmail.com>
2021-08-20 10:52:47 +0900

    delete count in  coupons_controller

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -3,7 +3,6 @@ class CouponsController < ApplicationController
   # GET /welcome
   def create
     users = User.where(enable: true).pluck(:line_id)
-    count = users.count
     users.each do |user|
       response = GajoenApi.create_tickets(brand_id: 145, item_id: 56509)
       message = {
@@ -11,10 +10,8 @@ class CouponsController < ApplicationController
         text: "日頃の感謝を込めてクーポンを配信します！是非お使いください！\n#{response['url']}"
       }
       client.push_message(user, message)
-      count = count - 1
     end
     flash[:notice] = "#{users.count}件クーポンを配信しました。"
     redirect_to controller: :welcome
-
   end
 end
